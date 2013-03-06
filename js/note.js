4e902d409cdd833a8dac51b40490bb072b8cadb5
Francesco Trillini <francescotrillini@gmail.com>
2013-03-06 15:35:52 +0100

    Update note.js

--- js/note.js
+++ js/note.js
@@ -1,5 +1,5 @@
 /*
- * Copyright MIT � <2013> <Francesco Trillini>
+ * Copyright MIT © <2013> <Francesco Trillini>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
